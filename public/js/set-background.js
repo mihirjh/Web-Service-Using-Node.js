28cfb2e38c0bea6517679dec5c01bc21195801f3
mihirjh <mihirkjha@gmail.com>
2018-02-15 18:29:29 +0800

    REST API invocation

--- public/js/set-background.js
+++ public/js/set-background.js
@@ -1,3 +1,9 @@
 var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
+
+function getResponse(){
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://41mkwllvad.execute-api.us-east-2.amazonaws.com/Prod/mihir/Jha)", false(async flag), "userName", "password");
+  request.send();
+}
